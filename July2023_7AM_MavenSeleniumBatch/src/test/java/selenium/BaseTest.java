0232c3583f82dc1799a8d49e56728728c0ed3b19
ravilella1234 <masters.selenium@gmail.com>
2023-07-14 07:57:51 +0530

    Propertie Files & added selenium documents

--- July2023_7AM_MavenSeleniumBatch/src/test/java/selenium/BaseTest.java
+++ July2023_7AM_MavenSeleniumBatch/src/test/java/selenium/BaseTest.java
@@ -1,5 +1,10 @@
 package selenium;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -10,16 +15,28 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseTest 
 {
 	public static WebDriver driver;
+	public static Properties p;
+	
+	public static void init() throws Exception
+	{
+		System.out.println(System.getProperty("user.dir"));
+		//FileInputStream fis = new FileInputStream("D:\\April2022WD\\July2023_7AM_MavenSeleniumBatch\\src\\test\\resources\\data.properties");
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\data.properties");
+		p = new Properties();
+		p.load(fis);
+		//String e = p.getProperty("chromebrowser");
+		//System.out.println(e);
+	}
 	
 	public static void launch(String browserType)
 	{
-		if(browserType.equalsIgnoreCase("chrome")) {
+		if(p.getProperty(browserType).equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
-		}else if(browserType.equals("firefox")) {
+		}else if(p.getProperty(browserType).equals("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
-		}else if(browserType.equals("ie")) {
+		}else if(p.getProperty(browserType).equals("ie")) {
 			WebDriverManager.iedriver().setup();
 			driver = new InternetExplorerDriver();
 		}
@@ -27,8 +44,8 @@ public class BaseTest
 	
 	public static void navigateUrl(String url)
 	{
-		driver.get(url);
-		driver.navigate().to(url);
+		driver.get(p.getProperty(url));
+		//driver.navigate().to(url);
 	}
 
 }
---
+++ July2023_7AM_MavenSeleniumBatch/src/test/java/selenium/TC_003.java
@@ -0,0 +1,15 @@
+package selenium;
+
+public class TC_003 extends BaseTest
+{
+
+	public static void main(String[] args) throws Exception 
+	{
+		init();
+		
+		launch("chromebrowser");
+		
+		navigateUrl("amazonurl");
+	}
+
+}
