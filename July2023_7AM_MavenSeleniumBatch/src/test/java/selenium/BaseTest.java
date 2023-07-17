0bd0980e3c65709a4e333316757fb89e15dc1791
ravilella1234 <masters.selenium@gmail.com>
2023-07-17 07:53:48 +0530

    How to load different instance url applications by using Properties class

--- July2023_7AM_MavenSeleniumBatch/src/test/java/selenium/BaseTest.java
+++ July2023_7AM_MavenSeleniumBatch/src/test/java/selenium/BaseTest.java
@@ -15,17 +15,31 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseTest 
 {
 	public static WebDriver driver;
+	public static FileInputStream fis;
+	public static String projectPath = System.getProperty("user.dir");
 	public static Properties p;
+	public static Properties mainProp;
+	public static Properties childProp;
 	
 	public static void init() throws Exception
 	{
 		System.out.println(System.getProperty("user.dir"));
 		//FileInputStream fis = new FileInputStream("D:\\April2022WD\\July2023_7AM_MavenSeleniumBatch\\src\\test\\resources\\data.properties");
-		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\test\\resources\\data.properties");
+		 fis = new FileInputStream(projectPath +"\\src\\test\\resources\\data.properties");
 		p = new Properties();
 		p.load(fis);
-		//String e = p.getProperty("chromebrowser");
-		//System.out.println(e);
+		
+		fis = new FileInputStream(projectPath+"\\src\\test\\resources\\environment.properties");
+		mainProp = new Properties();
+		mainProp.load(fis);
+		String e = mainProp.getProperty("env");
+		System.out.println(e);
+		
+		fis = new FileInputStream(projectPath+"\\src\\test\\resources\\"+e+".properties");
+		childProp = new Properties();
+		childProp.load(fis);
+		String url = childProp.getProperty("amazonurl");
+		System.out.println(url);
 	}
 	
 	public static void launch(String browserType)
@@ -44,7 +58,7 @@ public class BaseTest
 	
 	public static void navigateUrl(String url)
 	{
-		driver.get(p.getProperty(url));
+		driver.get(childProp.getProperty(url));
 		//driver.navigate().to(url);
 	}
 
