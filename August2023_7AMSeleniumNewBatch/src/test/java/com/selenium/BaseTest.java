38f0e0ae1a207a796149135d664fd6ba226d2e50
ravilella1234 <masters.selenium@gmail.com>
2023-08-21 08:15:51 +0530

    Properties Class

--- August2023_7AMSeleniumNewBatch/src/test/java/com/selenium/BaseTest.java
+++ August2023_7AMSeleniumNewBatch/src/test/java/com/selenium/BaseTest.java
@@ -1,5 +1,9 @@
 package com.selenium;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
@@ -11,22 +15,47 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseTest 
 {
 	public static WebDriver driver;
+	public static String projectPath = System.getProperty("user.dir");
+	public static Properties p;
+	public static Properties mainProp;
+	public static Properties childProp;
+	public static FileInputStream fis;
+	
+	public static void init() throws Exception
+	{
+		System.out.println(projectPath);
+		fis = new FileInputStream(projectPath+"\\src\\test\\resources\\data.properties");
+		p = new Properties();
+		p.load(fis);
+		
+		fis = new FileInputStream(projectPath+"\\src\\test\\resources\\environment.properties");
+		mainProp = new Properties();
+		mainProp.load(fis);
+		String e = mainProp.getProperty("env");
+		System.out.println(e);
+		
+		fis = new FileInputStream(projectPath+"\\src\\test\\resources\\"+e+".properties");
+		childProp = new Properties();
+		childProp.load(fis);
+		String url = childProp.getProperty("amazonurl");
+		System.out.println(url);
+	}
 	
 	public static void launch(String browser)
 	{
-		if(browser.equalsIgnoreCase("chrome"))
+		if(p.getProperty(browser).equalsIgnoreCase("chrome"))
 		{
 			WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 		}
-		else if(browser.equals("firefox"))
+		else if(p.getProperty(browser).equals("firefox"))
 		{
 			FirefoxOptions option = new FirefoxOptions();
 			option.setBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe");
 			System.setProperty("webdriver.gecko.driver", "C:\\Users\\ravi\\Downloads\\geckodriver-v0.33.0-win64\\geckodriver.exe");
 			driver = new FirefoxDriver(option);
 		}
-		else if(browser.equals("edge"))
+		else if(p.getProperty(browser).equals("edge"))
 		{
 			WebDriverManager.edgedriver().setup();
 			driver = new EdgeDriver();
@@ -35,7 +64,7 @@ public class BaseTest
 	
 	public static void navigateUrl(String url)
 	{
-		driver.get(url);
+		driver.get(childProp.getProperty(url));
 		//driver.navigate().to(url);
 	}
 
