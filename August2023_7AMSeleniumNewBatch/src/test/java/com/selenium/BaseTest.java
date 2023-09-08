33f3e23056a651a97a79358641ec3392d9126b8e
ravilella1234 <masters.selenium@gmail.com>
2023-09-08 08:23:51 +0530

    WebDriver Screenshots & Verifications

--- August2023_7AMSeleniumNewBatch/src/test/java/com/selenium/BaseTest.java
+++ August2023_7AMSeleniumNewBatch/src/test/java/com/selenium/BaseTest.java
@@ -1,22 +1,28 @@
 package com.selenium;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.time.Duration;
+import java.util.Date;
 import java.util.Properties;
 
 import org.apache.log4j.PropertyConfigurator;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -183,6 +189,37 @@ public class BaseTest
 		}
 		
 		return by;
+	}
+	
+	public static void reportPass(String passMessage) 
+	{
+		test.log(Status.PASS, passMessage);
+	}
+
+	public static void reportFailure(String failMessgae) throws Exception 
+	{
+		test.log(Status.FAIL, failMessgae);
+		takesScreenshot();
+	}
+
+	public static void takesScreenshot() throws Exception
+	{
+		Date dt=new Date();
+		System.out.println(dt);
+		String dateFormat=dt.toString().replace(":", "_").replace(" ", "_")+".png";		
+		File scrFile=((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(scrFile, new File(System.getProperty("user.dir")+"//failurescreenshots//"+dateFormat));
+		
+		test.log(Status.INFO,"Screenshot --->" +test.addScreenCaptureFromPath(System.getProperty("user.dir")+"//failurescreenshots//"+dateFormat));
+	}
+
+	public static boolean isLinksEqual(String expectedLink) 
+	{
+		String actualLink = driver.findElement(By.linkText("Gmail")).getAttribute("innerHTML");
+		if(actualLink.equals(expectedLink))
+			return true;
+		else
+			return false;
 	}
 
 }
